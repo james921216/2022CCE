05353a879499322e1c45796d5efb2640eb6e5660
james921216 <921216james@gmail.com>
2022-12-20 11:58:32 +0800

    week16

---
+++ week16/week16-1.cpp
@@ -0,0 +1,17 @@
+///狡策 scanf()
+#include <stdio.h>
+///量秆stdio.h 琌或
+
+int main()
+{
+        printf("叫块计ダ\n");
+        int a;
+        scanf("%d", &a);
+
+        char c;
+        scanf("%c", &c);
+
+        printf("计琌:%d ダ琌%c", a, c);
+
+}
+
